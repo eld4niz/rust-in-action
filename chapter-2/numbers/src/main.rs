f47eb281c8fd7dfdbe818985a36f0c566dcdb8bf
eld4niz <eldanizbabayev@protonmail.com>
2024-04-15 19:03:07 +0200

    Revert "README.md added, chapter 2 updated"
    This reverts commit 10a109968c406d43092e79c2b62ccca9aab16eac.

--- chapter-2/numbers/src/main.rs
+++ chapter-2/numbers/src/main.rs
@@ -1,6 +1,4 @@
-use std::convert::TryInto;
-
-fn numeric_literals() {
+fn main() {
     let twenty = 20;
     let twenty_one: i32 = 21;
     let twenty_two = 22i32;
@@ -9,37 +7,4 @@ fn numeric_literals() {
     println!("twenty + twenty_one + twenty_two = {}", addition);
 
     let one_million: i64 = 1_000_000;
-    println!("{}", one_million.pow(2));
-
-    let forty_twos = [42.0, 42f32, 42.0_f32];
-
-    println!("{:02}", forty_twos[0]);
-}
-
-fn using_base() {
-    let three = 0b11;
-    let thirty = 0o36;
-    let three_hundred = 0x12C;
-
-    println!("base 10: {}, {}, {}", three, thirty, three_hundred);
-    println!("base 2: {:b}, {:b}, {:b}", three, thirty, three_hundred); // binary
-    println!("base 8: {:o}, {:o}, {:o}", three, thirty, three_hundred); // octal
-    println!("base 16: {:x}, {:x}, {:x}", three, thirty, three_hundred); // hexadecimal
-}
-
-fn try_into_example() {
-    let a: i32 = 300;
-    let b: u16 = 100;
-
-    let b_ = b.try_into().unwrap();
-
-    if a < b_ {
-        println!("hundred is lett than three hundred");
-    }
-}
-
-fn main() {
-    numeric_literals();
-    using_base();
-    try_into_example();
 }
